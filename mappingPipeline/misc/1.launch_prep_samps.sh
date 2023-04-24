6bcf70bf723f2ff800b030f15a8fdb96e7203e19
Joaquin Nunez <jcbnunez@JCBN2020.local>
2023-04-24 14:36:10 -0400

    misc

---
+++ mappingPipeline/misc/1.launch_prep_samps.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#
+#SBATCH -J blsmm_env # A single job name for the array
+#SBATCH --ntasks-per-node=5 # ten cores
+#SBATCH -N 1 # on one node
+#SBATCH -t 5:00:00 ### 20 minutes per job per 10 permutations
+#SBATCH --mem 40G
+#SBATCH -p standard
+#SBATCH --account berglandlab
+#SBATCH --array=1-16
+
+module load gcc/7.1.0 openmpi/3.1.4 R/4.1.1 gdal proj
+
+Rscript \
+--vanilla \
+seasonality.R \
+${SLURM_ARRAY_TASK_ID}
+
+
