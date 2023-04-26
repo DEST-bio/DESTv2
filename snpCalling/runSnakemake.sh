e034668146e6e7f4bd7e11906aba59bbda1f9bc0
Alan Bergland <aob2x@virginia.edu>
2023-04-26 16:50:45 -0400

    Update runSnakemake.sh

--- snpCalling/runSnakemake.sh
+++ snpCalling/runSnakemake.sh
@@ -5,10 +5,13 @@
 #SBATCH -N 1 # on one node
 #SBATCH -t 80:00:00 ### 1 hours
 #SBATCH --mem 1G
-#SBATCH -o /scratch/aob2x/DESTv2_output_SNAPE/logs/runSnakemake.%A_%a.out # Standard output
-#SBATCH -e /scratch/aob2x/DESTv2_output_SNAPE/logs/runSnakemake.%A_%a.err # Standard error
+#SBATCH -o /scratch/aob2x/DESTv2_output_26April2023/logs/runSnakemake.%A_%a.out # Standard output
+#SBATCH -e /scratch/aob2x/DESTv2_output_26April2023/logs/runSnakemake.%A_%a.err # Standard error
 #SBATCH -p standard
 #SBATCH --account berglandlab_standard
+
+### cat /scratch/aob2x/DESTv2_output_SNAPE/logs/runSnakemake.49369837*.err
+
 
 
 module load gcc/9.2.0 openmpi/3.1.6 python/3.7.7 snakemake/6.0.5
