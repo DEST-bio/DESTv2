f05b1d03d59f6cd816c0636b37fd59a68f9f0f71
Alan Bergland <aob2x@virginia.edu>
2023-04-29 13:26:18 -0400

    Update manual_gather.sh

--- snpCalling/scatter_gather_annotate/manual_gather.sh
+++ snpCalling/scatter_gather_annotate/manual_gather.sh
@@ -18,7 +18,7 @@
 
 module purge
 
-module load htslib/1.10.2 bcftools/1.9 intel/18.0 intelmpi/18.0 parallel/20200322
+module load htslib/1.10.2 bcftools/1.9 intel/18.0 intelmpi/18.0 parallel/20200322 vcftools/0.1.16
 
 concatVCF() {
 
@@ -38,21 +38,35 @@ concatVCF() {
   if [ ! -d $bcf_outdir ]; then
       mkdir $bcf_outdir
   fi
-#
+
   outdir=$wd/sub_vcfs
   cd ${outdir}
-#
+
   echo "generate list"
-  ls -d *.${popSet}.${method}.${maf}.${mac}.${version}.norep.vcf.gz | grep ${chr} | sort -t"_" -k2n,2 -k4g,4 \
+  #ls -d *.${popSet}.${method}.${maf}.${mac}.${version}.norep.vcf.gz | grep '^${chr}_' | sort -t"_" -k2n,2 -k4g,4 \
+  #> $outdir/vcfs_order.${chr}.${popSet}.${method}.${maf}.${mac}.${version}.sort
+
+
+  ls -d *.${popSet}.${method}.${maf}.${mac}.${version}.norep.vcf.gz | grep "^${chr}_"| sort -t"_" -k2n,2 -k4g,4
   > $outdir/vcfs_order.${chr}.${popSet}.${method}.${maf}.${mac}.${version}.sort
-#
+
+
   echo "Concatenating"
-#
-  bcftools concat \
+
+  #bcftools concat \
+  #-f $outdir/vcfs_order.${chr}.${popSet}.${method}.${maf}.${mac}.${version}.sort \
+  #-O z \
+  #-n \
+  #-o $bcf_outdir/dest.${chr}.${popSet}.${method}.${maf}.${mac}.${version}.norep.vcf.gz
+
+  vcf-concat \
   -f $outdir/vcfs_order.${chr}.${popSet}.${method}.${maf}.${mac}.${version}.sort \
-  -O z \
-  -n \
-  -o $bcf_outdir/dest.${chr}.${popSet}.${method}.${maf}.${mac}.${version}.norep.vcf.gz
+  -s |
+  bgzip -c -i - > $bcf_outdir/dest.${chr}.${popSet}.${method}.${maf}.${mac}.${version}.norep.vcf.gz
+
+  tabix -p vcf $bcf_outdir/dest.${chr}.${popSet}.${method}.${maf}.${mac}.${version}.norep.vcf.gz
+
+
 }
 export -f concatVCF
 
