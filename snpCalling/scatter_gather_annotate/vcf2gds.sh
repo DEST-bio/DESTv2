e26214593cd91cd610c118b6526fff88d5c465fa
Alan Bergland <aob2x@virginia.edu>
2023-06-08 20:11:50 -0400

    Update vcf2gds.sh

--- snpCalling/scatter_gather_annotate/vcf2gds.sh
+++ snpCalling/scatter_gather_annotate/vcf2gds.sh
@@ -3,7 +3,7 @@
 #SBATCH -J vcf2gds # A single job name for the array
 #SBATCH --ntasks-per-node=10 # one core
 #SBATCH -N 1 # on one node
-#SBATCH -t 0-48:00  ### 48 hours
+#SBATCH -t 0:02:00  ### 48 hours
 #SBATCH --mem 24G
 #SBATCH -o /scratch/aob2x/dest/slurmOutput/vcf2gds.%A_%a.out # Standard output
 #SBATCH -e /scratch/aob2x/dest/slurmOutput/vcf2gds.%A_%a.err # Standard error
@@ -11,7 +11,7 @@
 #SBATCH --account berglandlab_standard
 
 ### sbatch /scratch/aob2x/DESTv2/snpCalling/scatter_gather_annotate/vcf2gds.sh
-### sacct -j 50241702
+### sacct -j 50241880
 ### cat /scratch/aob2x/dest/slurmOutput/vcf2gds.22867938
 
 module load gcc/7.1.0 openmpi/3.1.4 R/3.6.3
