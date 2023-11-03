f165edbd2468780bd59a6bc6e5ae360e57d828a3
Alan Bergland <aob2x@virginia.edu>
2023-11-03 09:31:45 -0400

    Update get_unmaped_microbiome.sh

--- examples/subnset_bam/get_unmaped_microbiome.sh
+++ examples/subnset_bam/get_unmaped_microbiome.sh
@@ -11,9 +11,9 @@
 #SBATCH --account biol4559-aob2x
 
 ### Nov 2 2023
-### run as: sbatch --array=1-743 /scratch/aob2x/scratch/aob2x/DESTv2/examples/subnset_bam/get_unmaped_microbiome.sh
+### run as: sbatch --array=1-743 /scratch/aob2x/DESTv2/examples/subnset_bam/get_unmaped_microbiome.sh
 
-### sacct -j 54723955
+### sacct -j 54781339
 ### cat /scratch/aob2x/logs/dest_fst.54723955_46.out
 ### SLURM_ARRAY_TASK_ID=2
 
@@ -27,17 +27,17 @@
   fileStem=$( echo $inputFile | rev | cut -f1 -d'/' | rev )
 
 ### get unmapped reads
-  samtools view -h -@ 20 -b -f 4 $inputFile  > /scratch/aob2x/DESTv2_unmapped_reads/unmapped.${fileStem}
+  samtools view -h -@ 20 -b -f 4 $inputFile  > /scratch/aob2x/DESTv2_unmapped_reads_v2/unmapped.${fileStem}
 
 ### get reads mapping to non-Drosophila genomes
   # samtools idxstats ${inputFile} | grep -vE "2L|2R|3L|3R|4|X|Y|mitochondrion_genome|sim_2L|sim_2R|sim_3L|sim_3R|sim_4|sim_X|sim_mtDNA" | cut -f1,2 | awk '{print $1"\t"1"\t"$2}' > /scratch/aob2x/DESTv2_unmapped_reads/nonDrosGenome.bed
   # sed -i '$d' /scratch/aob2x/DESTv2_unmapped_reads/nonDrosGenome.bed
 
-  samtools view -@ 20 $inputFile $sim_chromosomes -b > /scratch/aob2x/DESTv2_unmapped_reads/nonDros.${fileStem}
+  samtools view -@ 20 -L /scratch/aob2x/DESTv2_unmapped_reads_v2/nonDrosGenome.bed $inputFile $sim_chromosomes -b > /scratch/aob2x/DESTv2_unmapped_reads/nonDros.${fileStem}
 
 ### index
-  samtools index /scratch/aob2x/DESTv2_unmapped_reads/unmapped.${fileStem}
-  samtools index /scratch/aob2x/DESTv2_unmapped_reads/nonDros.${fileStem}
+  samtools index /scratch/aob2x/DESTv2_unmapped_reads_v2/unmapped.${fileStem}
+  samtools index /scratch/aob2x/DESTv2_unmapped_reads_v2/nonDros.${fileStem}
 
 ### log
-  ls -lh /scratch/aob2x/DESTv2_unmapped_reads/*
+  ls -lh /scratch/aob2x/DESTv2_unmapped_reads_v2/*
