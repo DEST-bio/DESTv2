9b0b600f585bd0d090685f24ce61065e9afa1161
Alan Bergland <aob2x@virginia.edu>
2023-11-28 08:18:15 -0500

    remap

--- mappingPipeline/misc/remap_for_unmapped.sh/remap_dest.localFASTQ.sh
+++ mappingPipeline/misc/remap_for_unmapped.sh/remap_dest.localFASTQ.sh
@@ -3,8 +3,8 @@
 #SBATCH -J remap_fastq # A single job name for the array
 #SBATCH --ntasks-per-node=10 # one core
 #SBATCH -N 1 # on one node
-#SBATCH -t 15:00:00 ### 6 hours
-#SBATCH --mem 75G
+#SBATCH -t 10:00:00 ### 6 hours
+#SBATCH --mem 50G
 #SBATCH -o /scratch/aob2x/dest/slurmOutput/remap.%A_%a.out # Standard output
 #SBATCH -e /scratch/aob2x/dest/slurmOutput/remap.%A_%a.err # Standard error
 #SBATCH -p standard
@@ -12,15 +12,11 @@
 
 wd=/scratch/aob2x/dest
 ### grep -E "ES_ba_12|AT_gr_12" /scratch/aob2x/dest/DEST/populationInfo/samps.csv | cut -f1,13 -d',' > /scratch/aob2x/fastq/todl.csv
-### run as: sbatch --array=592,626,694,710,711,714,716,718,719,721,722,746 /scratch/aob2x/DESTv2/mappingPipeline/misc/remap_for_unmapped.sh/remap_dest.sh
-### sacct -j 55440483
-### cat /scratch/aob2x/dest/slurmOutput/remap.55440483_711.err
+### run as: sbatch --array=2-754%10 /scratch/aob2x/DESTv2/mappingPipeline/misc/remap_for_unmapped.sh/remap_dest.sh
+### sacct -j 55233826
+### cat /scratch/aob2x/dest/slurmOutput/remap.55114839_2.err
 
-###   samtools idxstats /project/berglandlab/DEST/dest_mapped/Cville/US_Vir_Cha_1_2016-07-08/US_Vir_Cha_1_2016-07-08.original.bam | grep -vE "2L|2R|3L|3R|4|X|Y|mitochondrion_genome|sim_2L|sim_2R|sim_3L|sim_3R|sim_4|sim_X|sim_mtDNA" | cut -f1,2 | awk '{print $1"\t"1"\t"$2}' > /scratch/aob2x/DESTv2_unmapped_reads/nonDrosGenome.bed
-###   sed -i '$d' /scratch/aob2x/DESTv2_unmapped_reads/nonDrosGenome.bed
-
-
-module load sratoolkit/2.10.5 samtools/1.9 gcc/9.2.0 bwa/0.7.17 picard/2.23.4 cutadapt/3.4
+module load sratoolkit/2.10.5 samtools/1.9 gcc/9.2.0 bwa/0.7.17 picard/2.23.4 cutadapt/3.4 openmpi globus_cli
 threads=10
 
 #SLURM_ARRAY_TASK_ID=2
@@ -31,27 +27,7 @@ threads=10
 
   echo $sample " / " $sranum
 
-### download if necesary
-  if [ ! -f "/scratch/aob2x/dest/fastq/${sranum}.sra" ]; then
-    prefetch \
-    -o /scratch/aob2x/dest/fastq/${sranum}.sra \
-    ${sranum}
-  fi
-
-  if [ ! -f "/scratch/aob2x/dest/fastq/${sranum}_1.fastq" ]; then
-
-    fasterq-dump \
-    --split-files \
-    --split-3 \
-    --outfile /scratch/aob2x/dest/fastq/${sranum} \
-    -e 10 \
-    /scratch/aob2x/dest/fastq/${sranum}.sra
-
-    # gzip /scratch/aob2x/dest/fastq/${sranum}_1.fastq
-    # gzip /scratch/aob2x/dest/fastq/${sranum}_2.fastq
-
-    # rm /scratch/aob2x/fastq/${sranum}.sra
-  fi
+### copy over
 
 ### trim
   if [ ! -f /scratch/aob2x/dest/fastq/${sranum}.trimmed1.fq ]; then
@@ -60,15 +36,18 @@ threads=10
     cutadapt \
     -q 18 \
     --minimum-length 25 \
-    -o /scratch/aob2x/dest/fastq/${sranum}.trimmed1.fq \
-    -p /scratch/aob2x/dest/fastq/${sranum}.trimmed2.fq \
+    -o /scratch/aob2x/dest/fastq/${sranum}.trimmed1.fq.gz \
+    -p /scratch/aob2x/dest/fastq/${sranum}.trimmed2.fq.gz \
     -b ACACTCTTTCCCTACACGACGCTCTTCCGATC \
     -B CAAGCAGAAGACGGCATACGAGAT \
     -O 15 \
     -n 3 \
     --cores=$threads \
-    /scratch/aob2x/dest/fastq/${sranum}_1.fastq \
-    /scratch/aob2x/dest/fastq/${sranum}_2.fastq
+    /project/berglandlab/DEST/raw_reads/DrosEU_3_Jan2023/${sranum}_1.fastq.gz \
+    /project/berglandlab/DEST/raw_reads/DrosEU_3_Jan2023/${sranum}_2.fastq.gz
+
+    gunzip /scratch/aob2x/dest/fastq/${sranum}.trimmed1.fq.gz
+    gunzip /scratch/aob2x/dest/fastq/${sranum}.trimmed2.fq.gz
   fi
 
   echo "cutadapt done"
@@ -146,7 +125,3 @@ threads=10
   rm /scratch/aob2x/dest/bam/${sample}.merged.bam
   rm /scratch/aob2x/dest/bam/${sample}.merged_un.bam
   rm /scratch/aob2x/dest/bam/${sample}.sorted_merged.bam
-  rm /scratch/aob2x/dest/bam/${sample}.merged_un.bam
-
-
-  ls -lh /scratch/aob2x/dest/bam/*sorted_merged.nonDros.bam | grep -v "54K"
